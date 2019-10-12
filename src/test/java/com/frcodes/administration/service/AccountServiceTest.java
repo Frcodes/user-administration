59751a8aef0c50f6b869e6e2e9f3fee2a445720b
FrCodes <currocodes@gmail.com>
2019-10-12 10:36:44 +0200

    + Add tests definitions for services

---
+++ src/test/java/com/frcodes/administration/service/AccountServiceTest.java
@@ -0,0 +1,30 @@
+package com.frcodes.administration.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AccountServiceTest {
+
+	
+
+
+	@Autowired
+	private AccountService accountService;
+
+	@Test
+	public void createUserAccount() {
+
+		// TODO
+	}
+	
+	@Test
+	public void listAccount() {
+
+		// TODO
+	}
+}
